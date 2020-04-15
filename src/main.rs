e40004b382fa2bd76370cb5981ef1a52fb4fae51
Addison Bean <addisonbean@gmail.com>
2020-04-15 14:01:59 -0500

    Add better info after receiving a ping

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,14 @@ use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+
+#[derive(Clone, Copy, Default)]
+struct PingStats {
+    num_sent: u64,
+    num_received: u64,
+    total_rtt: u128,
+}
 
 fn make_ping_request<'a>(data: &'a mut [u8]) -> MutableEchoRequestPacket<'a> {
     let mut req = MutableEchoRequestPacket::new(data).unwrap();
@@ -33,6 +40,18 @@ fn make_ping_request<'a>(data: &'a mut [u8]) -> MutableEchoRequestPacket<'a> {
 fn send_ping(hostname: IpAddr, data: &mut [u8], sender: &mut TransportSender) -> io::Result<usize> {
     let req = make_ping_request(data);
     sender.send_to(req, hostname)
+}
+
+
+fn print_stats_for_rtt(rtt: u128, stats: PingStats) {
+    let avg_rtt = stats.total_rtt as f64 / stats.num_received as f64;
+    let total_loss = 1.0 - stats.num_received as f64 / stats.num_sent as f64;
+    println!(
+        "Response received: {}ms rtt, {:.2} average rtt, {:.2}% total loss",
+        rtt,
+        avg_rtt,
+        total_loss,
+    );
 }
 
 fn main() -> io::Result<()> {
@@ -57,7 +76,6 @@ fn main() -> io::Result<()> {
     let (mut sender, mut receiver) = transport_channel(4096, protocol)?;
     sender.set_ttl(64)?;
 
-
     let stop_signal_sent = Arc::new(AtomicBool::new(false));
     let stop_signal_sent_handler = Arc::clone(&stop_signal_sent);
     ctrlc::set_handler(move || {
@@ -67,8 +85,7 @@ fn main() -> io::Result<()> {
     let mut packet_iter = icmp_packet_iter(&mut receiver);
     let mut data = [0; MutableEchoRequestPacket::minimum_packet_size()];
 
-    let mut num_sent = 0;
-    let mut num_received = 0;
+    let mut stats = PingStats::default();
 
     // TODO: check for failed parse
     let hostname = hostname.parse().unwrap();
@@ -76,15 +93,19 @@ fn main() -> io::Result<()> {
         if stop_signal_sent.load(atomic::Ordering::SeqCst) { break }
 
         send_ping(hostname, &mut data, &mut sender)?;
-        num_sent += 1;
+        let time_sent = Instant::now();
+        stats.num_sent += 1;
 
         // The unix ping program can be terminated here...
         // It doesn't wait for a response to terminate and print the summary
         if stop_signal_sent.load(atomic::Ordering::SeqCst) { break }
 
         let res = packet_iter.next();
-        println!("{:?}", res);
-        num_received += 1;
+        stats.num_received += 1;
+        let rtt = Instant::now().duration_since(time_sent).as_millis();
+        stats.total_rtt += rtt;
+
+        print_stats_for_rtt(rtt, stats);
 
         // TODO: can I allow it to be interrupted from sleeping and break immediately?
         thread::sleep(Duration::from_millis(500));
@@ -92,8 +113,8 @@ fn main() -> io::Result<()> {
 
     // -- Print summary
 
-    println!("Num sent: {}", num_sent);
-    println!("Num recv: {}", num_received);
+    // println!("Num sent: {}", num_sent);
+    // println!("Num recv: {}", num_received);
 
     Ok(())
 }
